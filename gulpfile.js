1f63cbcdf741cdff3bac47bc837eaaba2cdc2f46
Jonathan Sudiaman <jsudiaman@gmail.com>
2017-11-20 22:03:19 -0500

    Using Gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp')
+var bro = require('gulp-bro')
+var concat = require('gulp-concat')
+var rename = require('gulp-rename')
+
+gulp.task('browserify', function () {
+  gulp.src('js/main.js')
+    .pipe(bro())
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('dist/js'))
+})
+
+gulp.task('concat-css', function () {
+  return gulp.src(['css/style.css', 'node_modules/bootstrap/dist/css/bootstrap.min.css', 'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'])
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('copy-fonts', function () {
+  gulp.src('node_modules/bootstrap/dist/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts/'))
+})
+
+gulp.task('default', ['browserify', 'concat-css', 'copy-fonts'])
