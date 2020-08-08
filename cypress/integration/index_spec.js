834536535cc76b61c77ca4a828f8167d3402ce68
Jonathan <jonathan@sudicode.com>
2020-08-08 15:28:26 -0400

    Add manual entry tests.

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -2,7 +2,10 @@
 
 describe('Index Page', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit('/', {
+      onBeforeLoad: (win) => win.sessionStorage.clear(),
+    });
+    cy.reload();
   });
 
   describe('Examples', () => {
@@ -27,11 +30,39 @@ describe('Index Page', () => {
 
     examples.forEach((example) => {
       it(`should compute chance for ${example.title}`, () => {
-        cy.reload();
         cy.contains('Examples').click();
         cy.contains(example.title).click();
         cy.get('#results').should('contain.text', example.chance);
       });
+    });
+  });
+
+  describe('Compute', () => {
+    it('should compute chance for single card combos', () => {
+      cy.get('#name0').type('Final Countdown');
+      cy.get('#aid0').clear().type('3');
+      cy.get('#ar0').clear().type('1');
+
+      cy.contains('Deck Size').type('40');
+      cy.contains('Hand Size').type('5');
+      cy.contains('Compute').click();
+      cy.get('#results').should('contain.text', 'Combo: 1x Final Countdown').and('contain.text', '33.8%');
+    });
+
+    it('should compute chance for multi card combos', () => {
+      cy.get('#name0').type('Valhalla, Hall of the Fallen');
+      cy.get('#aid0').clear().type('3');
+      cy.get('#ar0').clear().type('1');
+      cy.contains('Add Row').click();
+
+      cy.get('#name1').type('Archlord Kristya');
+      cy.get('#aid1').clear().type('3');
+      cy.get('#ar1').clear().type('1');
+
+      cy.contains('Deck Size').type('40');
+      cy.contains('Hand Size').type('5');
+      cy.contains('Compute').click();
+      cy.get('#results').should('contain.text', 'Combo: 1x Valhalla, Hall of the Fallen, 1x Archlord Kristya').and('contain.text', '9.80%');
     });
   });
 });
